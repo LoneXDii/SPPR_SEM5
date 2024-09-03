28021c412e3944ee6d0f8ddb05f8d33eb9c1355d
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-09-03 23:37:11 +0300

    Created base project

---
+++ WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_253505_PAVLOVICH.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
