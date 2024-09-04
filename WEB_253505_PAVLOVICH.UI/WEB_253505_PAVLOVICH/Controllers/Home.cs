d291736d81eb3bfd87e5f655421bbe15d0c82998
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-09-04 13:16:38 +0300

    Completed lab 2

--- WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH/Controllers/Home.cs
+++ WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH/Controllers/Home.cs
@@ -18,81 +18,12 @@ namespace WEB_253505_PAVLOVICH.Controllers
                 new ListDemo {Id=2, Name="Item 2"},
                 new ListDemo {Id=3, Name="Item 3"}
             };
+
             var model = new IndexViewModel { 
                 ListItems = new SelectList(listData, "Id", "Name")
             };
 
-
             return View(model);
-        }
-
-        // GET: Home/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Home/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Home/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Home/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Home/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Home/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Home/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
---
+++ WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_253505_PAVLOVICH.ViewComponents;
+
+public class CartViewComponent : ViewComponent
+{
+    public CartViewComponent()
+    {
+    }
+
+    public Task<IViewComponentResult> InvokeAsync()
+    {
+        return Task.FromResult<IViewComponentResult>(View());
+    }
+}
