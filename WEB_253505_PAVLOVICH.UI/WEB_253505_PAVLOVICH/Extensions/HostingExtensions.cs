ba0e41fc599e12f2732a92b9354893219db91688
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-09-04 18:10:15 +0300

    lab 3 part 3

---
+++ WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH/Extensions/HostingExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace WEB_253505_PAVLOVICH.UI.Extensions;
+
+public static class HostingExtensions
+{
+    public static void RegisterCustomServices(this WebApplicationBuilder builder)
+    {
+    }
+}
--- WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH/Program.cs
+++ WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH/Program.cs
@@ -1,7 +1,10 @@
+using WEB_253505_PAVLOVICH.UI.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.RegisterCustomServices();
 
 var app = builder.Build();
 
