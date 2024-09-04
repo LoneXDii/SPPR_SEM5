5db203a1e2c77efa0c7e774418ebf8e99fdd3802
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-09-05 00:59:00 +0300

    lab 3 part 5

---
+++ WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WEB_253505_PAVLOVICH.UI.Services.CategoryService;
+using WEB_253505_PAVLOVICH.UI.Services.DeviceService;
+
+namespace WEB_253505_PAVLOVICH.UI.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly ICategoryService _categoryService;
+    private readonly IDeviceService _deviceService;
+
+    public ProductController(ICategoryService categoryService, IDeviceService deviceService)
+    {
+        _categoryService = categoryService;
+        _deviceService = deviceService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var deviceResponce = await _deviceService.GetDeviceListAsync(null);
+        if (!deviceResponce.Successfull)
+            return NotFound(deviceResponce.ErrorMessage);
+        return View(deviceResponce.Data);
+    }
+}
