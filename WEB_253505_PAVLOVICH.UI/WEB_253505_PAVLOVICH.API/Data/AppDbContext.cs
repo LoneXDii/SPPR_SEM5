9efa78e42c81e1b97b72c029e16938280b5e9372
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-09-06 21:05:34 +0300

    lab 4 part 2

---
+++ WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH.API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WEB_253505_PAVLOVICH.Domain.Entities;
+
+namespace WEB_253505_PAVLOVICH.API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Device> Devices { get; set; }
+}
