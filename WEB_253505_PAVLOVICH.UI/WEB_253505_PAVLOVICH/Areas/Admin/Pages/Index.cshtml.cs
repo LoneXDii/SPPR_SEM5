28aad42b248b9904acfbd4ddd130a70644d223df
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-10-03 16:24:04 +0300

    lab 7 part 3.1

--- WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH/Areas/Admin/Pages/Index.cshtml.cs
+++ WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH/Areas/Admin/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
 using WEB_253505_PAVLOVICH.Domain.Entities;
+using WEB_253505_PAVLOVICH.UI.Extensions;
 using WEB_253505_PAVLOVICH.UI.Services.DeviceService;
 
 namespace WEB_253505_PAVLOVICH.UI.Areas.Admin.Pages;
@@ -12,19 +14,36 @@ public class IndexModel : PageModel
     public IndexModel(IDeviceService deviceService)
     {
         _deviceService = deviceService;
-
-        var devices = _deviceService.GetDeviceListAsync(null).Result.Data;
-        for (int i = 1; i <= devices?.TotalPages; i++)
-        {
-            Devices.AddRange(_deviceService.GetDeviceListAsync(null, i).Result.Data!.Items);
-        }
     }
 
     [BindProperty]
     public List<Device> Devices { get;set; } = new();
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
 
-    public void OnGet()
+    public async Task<IActionResult> OnGetAsync(int pageNo = 1)
     {
-        
+        var response = await _deviceService.GetDeviceListAsync(null, pageNo);
+        if (response.Successfull)
+        {
+            Devices = response.Data.Items;
+            TotalPages = response.Data.TotalPages;
+            CurrentPage = pageNo;
+
+            if (Request.IsAjaxRequest())
+            {
+                return Partial("_PaginationPartial", new
+                {
+                    Admin = true,
+                    CurrentPage = CurrentPage,
+                    TotalPages = TotalPages,
+                    Devices = Devices
+                });
+            }
+
+            return Page();
+        }
+
+        return RedirectToPage("/Error");
     }
 }
---
+++ WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH/Extensions/HttpRequestExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB_253505_PAVLOVICH.UI.Extensions;
+
+public static class HttpRequestExtensions
+{
+    public static bool IsAjaxRequest(this HttpRequest request)
+    {
+        return request.Headers["X-Requested-With"] == "XMLHttpRequest";
+    }
+}
