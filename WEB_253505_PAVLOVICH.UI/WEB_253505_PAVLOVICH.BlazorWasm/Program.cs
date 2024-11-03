81b4aaa1b3cf8cb79add004f3a6488d3b6623de2
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-11-03 16:59:05 +0300

    Lab 12 part 4.5

--- WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH.BlazorWasm/Program.cs
+++ WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH.BlazorWasm/Program.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WEB_253505_PAVLOVICH.BlazorWasm;
+using WEB_253505_PAVLOVICH.BlazorWasm.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetSection("ApiUrl").Value) })
+    .AddScoped<IDataService, DataService>();
+        
 
 builder.Services.AddOidcAuthentication(options =>
 {
--- WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH.BlazorWasm/Services/DataService.cs
+++ WEB_253505_PAVLOVICH.UI/WEB_253505_PAVLOVICH.BlazorWasm/Services/DataService.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json;
 using WEB_253505_PAVLOVICH.Domain.Entities;
+using WEB_253505_PAVLOVICH.Domain.Models;
 
 namespace WEB_253505_PAVLOVICH.BlazorWasm.Services;
 
@@ -32,13 +36,90 @@ internal class DataService : IDataService
 
     public event Action DataLoaded;
 
-    public Task GetCategoryListAsync()
+    public async Task GetCategoryListAsync()
     {
-        throw new NotImplementedException();
+        var urlString = $"{_httpClient.BaseAddress.AbsoluteUri}api/Categories";
+        try
+        {
+            var response = await _httpClient.GetAsync(new Uri(urlString));
+            if (!response.IsSuccessStatusCode)
+            {
+                Success = false;
+                ErrorMessage = $"Error occured in fetching data: {response.StatusCode.ToString()}";
+            }
+
+            var data = await response.Content.ReadFromJsonAsync<ResponseData<List<Category>>>(_jsonSerializerOptions);
+
+            if (!data.Successfull)
+            {
+                Success = false;
+                ErrorMessage = data.ErrorMessage;
+            }
+
+            Success = true;
+            Categories = data.Data;
+            DataLoaded.Invoke();
+
+        }
+        catch (Exception ex)
+        {
+            Success = false;
+            ErrorMessage = $"Error occured in http client: {ex.Message}";
+        }
     }
 
-    public Task GetProductListAsync(int pageNo = 1)
+    public async Task GetProductListAsync(int pageNo = 1)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var route = new StringBuilder($"{_httpClient.BaseAddress.AbsoluteUri}api/devices/");
+
+            if (SelectedCategory is not null)
+            {
+                route.Append($"{SelectedCategory}/");
+            }
+
+            List<KeyValuePair<string, string>> queryData = new();
+
+            if (pageNo > 1)
+            {
+                queryData.Add(KeyValuePair.Create("pageNo", pageNo.ToString()));
+            }
+
+            if (!_pageSize.Equals("3"))
+            {
+                queryData.Add(KeyValuePair.Create("pageSize", _pageSize));
+
+            }
+           
+            if (queryData.Count > 0)
+            {
+                route.Append(QueryString.Create(queryData));
+            }
+
+            var response = await _httpClient.GetAsync(new Uri(route.ToString()));
+            if (!response.IsSuccessStatusCode)
+            {
+                Success = false;
+                ErrorMessage = $"Error occured in fetching data: {response.StatusCode.ToString()}";
+            }
+
+            var data = await response.Content.ReadFromJsonAsync<ResponseData<ProductListModel<Device>>>(_jsonSerializerOptions);
+            if (!data.Successfull)
+            {
+                Success = false;
+                ErrorMessage = data.ErrorMessage;
+            }
+
+            Success = true;
+            Devices = data.Data.Items;
+            CurrentPage = data.Data.CurrentPage;
+            TotalPages = data.Data.TotalPages;
+            DataLoaded.Invoke();
+        }
+        catch (Exception ex) {
+            Success = false;
+            ErrorMessage = $"Error occured in http client: {ex.Message}";
+        }
     }
 }
